829f11f9a5fa60411dc9b53efdcf2a958b1b0b7a
iamllcoolray <llcoolray24@gmail.com>
2021-04-21 09:05:02 -0400

    added every section => index.php

--- index.php
+++ index.php
@@ -20,11 +20,209 @@
 <body id="page-top">
   <?php include "includes/navigation.php";?>
   <?php include "includes/header.php";?>
-  <?php include "includes/sections/services.php";?>
-  <?php include "includes/sections/make_an_appointment.php";?>
-  <?php include "includes/sections/about.php";?>
-  <?php include "includes/sections/our_barbers.php";?>
-  <?php include "includes/sections/contact.php";?>
+  <!-- Services -->
+  <section id="services" class="scroll-container">
+   <div class="container scroll-element js-scroll fade-in-bottom">
+     <h2 class="py-4 ps-2 my-0">Services</h2>
+
+      <!-- Pricing -->
+      <div class="row text-center">
+        <div class="col-lg-4 mb-4">
+          <div class="card h-100">
+            <h4 class="card-header color-red">Haircuts</h4>
+            <div class="card-body">
+              <p class="card-text">
+                Clean Up <span class="px-2">$10</span> <i class="bi bi-clock"></i>15
+              </p>
+              <p class="card-text">
+                Clean Up & Beard Trim <span class="px-2">$10</span> <i class="bi bi-clock"></i>15
+              </p>
+              <p class="card-text">
+                Gentlemen's Cut <span class="px-2">$10</span> <i class="bi bi-clock"></i>15
+              </p>
+              <p class="card-text">
+                Gentlemen's Cut & Beard <span class="px-2">$10</span> <i class="bi bi-clock"></i>15
+              </p>
+              <p class="card-text">
+                Shampoo, Condition & Style <span class="px-2">$10</span> <i class="bi bi-clock"></i>15
+              </p>
+              <p class="card-text">
+                The Platinum Comb <span class="px-2">$10</span> <i class="bi bi-clock"></i>15
+              </p>
+              <p class="card-text">
+                Traditional Hot Shave <span class="px-2">$10</span> <i class="bi bi-clock"></i>15
+              </p>
+              <p class="card-text">
+                Young Gentlemen's Cut <span class="px-2">$10</span> <i class="bi bi-clock"></i>15
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 mb-4">
+          <div class="card h-100">
+            <h4 class="card-header color-red">Facials</h4>
+            <div class="card-body">
+              <p class="card-text">
+                Black Mask <span class="px-2">$8</span> <i class="bi bi-clock"></i>20
+              </p>
+              <p class="card-text">
+                Mint Mask <span class="px-2">$15</span> <i class="bi bi-clock"></i>25
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 mb-4">
+          <div class="card h-100">
+            <h4 class="card-header color-red">Hair Color</h4>
+            <div class="card-body">
+              <p class="card-text">
+                Beard Color <span class="px-2">$15</span> <i class="bi bi-clock"></i>25
+              </p>
+              <p class="card-text">
+                Hair Color <span class="px-2">$20</span> <i class="bi bi-clock"></i>30
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Make an Appointment -->
+  <section id="make-an-appointment" class="img-sizing">
+      <!-- Background image -->
+      <div class="text-center bg-image maa-img">
+        <div class="container scroll-container">
+          <h2 class="pt-4 ps-2 my-0 heading-white position-absolute scroll-element js-scroll fade-in-bottom">Make an Appointment</h2>
+        </div>
+        <div class=" mask-sizing mx-auto scroll-container" style="background-color: rgba(0, 0, 0, 0.6)">
+          <div class="d-flex justify-content-center align-items-center h-100 scroll-element js-scroll fade-in-bottom">
+            <div class="text-white">
+              <h3 class="mb-3">Schedule Your Next Appointment Today with Booker.com!</h3>
+              <a class="btn button-change color-red btn-lg" href="https://go.booker.com/thecombwestonbarbershop" target="_blank" role="button"><i class="bi bi-calendar3 pe-2"></i>Make an Appointment</a>
+            </div>
+          </div>
+        </div>
+      </div>
+  </section>
+  <!-- About -->
+  <section id="about" class="scroll-container">
+    <div class="container scroll-element js-scroll fade-in-bottom">
+      <h2 class="py-4 ps-2 my-0">About The Comb Weston</h2>
+
+      <div class="row">
+        <div class="col-lg-4 mb-4 text-center">
+          <img src="img/logo/thecombweston.jpg" alt="the comb weston" width="200" height="200" class="rounded-circle">
+        </div>
+        <div class="col-lg-4 mb-4 ms-auto">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Our Barbers -->
+  <section id="our-barbers" class="color-dark-blue scroll-container">
+    <div class="container scroll-element js-scroll fade-in-bottom">
+      <h2 class="py-4 ps-2 my-0">Our Barbers</h2>
+
+      <div class="row text-center">
+        <div class="col-lg-4 mb-4">
+          <img src="img/barbers/place-holder.jpg" alt="" width="200" height="225" class="rounded-circle pb-3">
+          <h4>Abe</h4>
+          <a href="https://www.facebook.com/thecombweston/" class="text-decoration-none" target="_blank">
+            <i class="bi bi-facebook s-link-bio"></i>
+          </a>
+          <a href="https://www.instagram.com/thecombweston/" class="text-decoration-none" target="_blank">
+            <i class="bi bi-instagram s-link-bio"></i>
+          </a>
+        </div>
+        <div class="col-lg-4 mb-4">
+          <img src="img/barbers/place-holder.jpg" alt="" width="200" height="225" class="rounded-circle pb-3">
+          <h4>Andres "DRE" Gonzalez</h4>
+          <a href="https://www.facebook.com/thecombweston/" class="text-decoration-none" target="_blank">
+            <i class="bi bi-facebook s-link-bio"></i>
+          </a>
+          <a href="https://www.instagram.com/thecombweston/" class="text-decoration-none" target="_blank">
+            <i class="bi bi-instagram s-link-bio"></i>
+          </a>
+        </div>
+        <div class="col-lg-4 mb-4">
+          <img src="img/barbers/place-holder.jpg" alt="" width="200" height="225" class="rounded-circle pb-3">
+          <h4>Jay</h4>
+          <a href="https://www.facebook.com/thecombweston/" class="text-decoration-none" target="_blank">
+            <i class="bi bi-facebook s-link-bio"></i>
+          </a>
+          <a href="https://www.instagram.com/thecombweston/" class="text-decoration-none" target="_blank">
+            <i class="bi bi-instagram s-link-bio"></i>
+          </a>
+        </div>
+        <div class="col-lg-4 mb-4">
+          <img src="img/barbers/place-holder.jpg" alt="" width="200" height="225" class="rounded-circle pb-3">
+          <h4>Kenny Costa</h4>
+          <a href="https://www.facebook.com/thecombweston/" class="text-decoration-none" target="_blank">
+            <i class="bi bi-facebook s-link-bio"></i>
+          </a>
+          <a href="https://www.instagram.com/thecombweston/" class="text-decoration-none" target="_blank">
+            <i class="bi bi-instagram s-link-bio"></i>
+          </a>
+        </div>
+        <div class="col-lg-4 mb-4">
+          <img src="img/barbers/place-holder.jpg" alt="" width="200" height="225" class="rounded-circle pb-3">
+          <h4>Lizzie</h4>
+          <a href="https://www.facebook.com/thecombweston/" class="text-decoration-none" target="_blank">
+            <i class="bi bi-facebook s-link-bio"></i>
+          </a>
+          <a href="https://www.instagram.com/thecombweston/" class="text-decoration-none" target="_blank">
+            <i class="bi bi-instagram s-link-bio"></i>
+          </a>
+        </div>
+        <div class="col-lg-4 mb-4">
+          <img src="img/barbers/place-holder.jpg" alt="" width="200" height="225" class="rounded-circle pb-3">
+          <h4>Travis</h4>
+            <a href="https://www.facebook.com/thecombweston/" class="text-decoration-none" target="_blank">
+              <i class="bi bi-facebook s-link-bio"></i>
+            </a>
+            <a href="https://www.instagram.com/thecombweston/" class="text-decoration-none" target="_blank">
+              <i class="bi bi-instagram s-link-bio"></i>
+            </a>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Contact -->
+  <section id="contact" class="color-black scroll-container">
+    <div class="container scroll-element js-scroll fade-in-bottom">
+      <h2 class="py-4 ps-2 my-0 text-white">Contact</h2>
+      <div class="row">
+        <div class="col-lg-4 mb-4 ms-auto">
+          <iframe scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=2816%20Weston%20Road%20+()&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" width="100%" height="300" frameborder="0"></iframe> <a href='https://embedmap.org/'>google maps for my website</a> <script type='text/javascript' src='https://embedmaps.com/google-maps-authorization/script.js?id=037c0cfc45b7f12a857b74c4a13fe19e09bbbbb7'></script>
+        </div>
+        <div class="col-lg-4 mb-4 ms-auto">
+          <p>
+            <i class="bi bi-telephone-fill text-color-red"></i><a href="tel:+1-954-595-2932" class="text-decoration-none text-color-gray"><span class="ps-2">(954) 595-2932</span></a>
+          </p>
+          <p>
+            <i class="bi bi-geo-alt-fill text-color-red"></i><a href="https://www.google.com/maps/place/The+Comb+Barbershop/@26.083444,-80.3716501,17z/data=!3m1!4b1!4m5!3m4!1s0x88d9a0b242fb2543:0x55b144b9e5d8e89e!8m2!3d26.0834392!4d-80.3694614?hl=en" class="text-decoration-none text-color-gray" target="_blank"><span class="ps-2">2816 Weston Road, Weston, FL, 33331</span></a>
+          </p>
+          <div class="row">
+            <div div class="col-lg-1">
+              <p><i class="bi bi-clock-fill text-color-red"></i></p>
+            </div>
+            <div div class="col-lg-4 w-75" style="color:#999;">
+              <p>Mon. &#3; <span class="text-color-red">Closed</span></p>
+              <p>Tue. &#3; 9:00 a.m. - 7:00 p.m.</p>
+              <p>Wed. &#3; 9:00 a.m. - 7:00 p.m.</p>
+              <p>Thu. &#3; 9:00 a.m. - 7:00 p.m.</p>
+              <p>Fri. &#3; 9:00 a.m. - 7:00 p.m.</p>
+              <p>Sat. &#3; 9:00 a.m. - 6:00 p.m.</p>
+              <p>Sun. &#3; <span class="text-color-red">Closed</span></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
   <?php include "includes/footer.php";?>
 
   <!-- Bootstrap Core JavaScript -->
